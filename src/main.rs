8efa0740b2aaff114006b85908cc9042e020c180
Ben Chung <ckfinite@gmail.com>
2025-10-07 14:32:52 -0400

    Cleanup example

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,21 @@
 #![no_std]
 #![no_main]
 
-use core::usize;
-use defmt::*;
+use core::{usize};
 use embassy_executor::Spawner;
 use embassy_rp::clocks::ClockConfig;
 use embassy_rp::config;
 use {defmt_rtt as _, panic_probe as _};
 
-const example:u32 = 16;
+#[cfg_attr(not(target_os = "macos"), link_section = ".data.example")]
+static EXAMPLE:[u32; 1] = [1];
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     let p = embassy_rp::init(config::Config::new(
         ClockConfig::system_freq(200_000_000).unwrap(),
     ));
-    if example != 16 {
-        crate::panic!();
+    if unsafe { EXAMPLE.as_ptr().read_volatile() != 1 } {
+        panic!();
     }
-
-    info!("Hello World! {:?}", example);
 }
